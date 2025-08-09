215099ec20966b39ec4a6b263e77e73427f238e3
Schomklin <kungchomklin@gmail.com>
2025-08-09 16:04:51 +0700

    InvoiceandCustomerClass

---
+++ src/DiscountPolicy.java
@@ -0,0 +1,11 @@
+/*
+ * 1.DiscountPolicy Interface
+ */
+public interface DiscountPolicy {
+
+    /*
+     * @param amount The original amount
+     * @return The amount after the discount is applied.
+     */
+    double applyDiscount(double amount);    
+}
---
+++ src/NoDiscountPolicy.java
@@ -0,0 +1,21 @@
+/*
+ * 3.NoDiscountPolicy Class
+ * This class represents a policy with no discount.
+ */
+public class NoDiscountPolicy implements DiscountPolicy {
+    @Override
+    public double applyDiscount(double amount) {
+        // TODO Auto-generated method stub
+        if (amount < 0) {
+            throw new IllegalArgumentException("Amount cannot be negative");
+        }
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return "No Discount";
+    }
+
+}
